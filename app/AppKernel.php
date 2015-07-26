31ae6d6253f26a9fafc0ff1b0612ec363fe88c19
LordKote <lordkote@gmail.com>
2015-07-26 13:57:18 +0100

    removed travis configuration, using environment variables

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,7 +23,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test', 'travis'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
